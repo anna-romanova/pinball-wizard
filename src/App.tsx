692c01af289fbfc79bb7a1c3abd5567d2361fe0d
anna.romanova <anna.romanova@clio.com>
2019-10-31 13:29:02 -0700

    add drawer

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,17 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 import PinballWizardRoutes from "./routes";
+import Drawer from "./Drawer";
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <PinballWizardRoutes />
-      </Router>
+      <div>
+        <Router>
+          <Drawer />
+          <PinballWizardRoutes />
+        </Router>
+      </div>
     )
   }
 }
